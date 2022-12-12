0315d69acf2e6c71cde7ba109437b55a44496749
KellyMark <kmark602@gmail.com>
2022-12-12 11:45:23 -0500

    commit tree 620b7cfca1d1288e60eb900449eb4bd2fb32d06a

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  if (ride < 400) {
+    return "This one is on me!";
+  }
+  else if (ride > 400 && ride < 2000) {
+    return "That will be twenty bucks.";
+  }
+  else if ( ride > 2000 && ride < 2500) {
+    return "I will gladly take your thirty bucks.";
+  }
+  else {
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  if (city === "NYC" || city === "New York City") {
+    return "Ok, sounds good."
+  }
+  else {
+    return "No go.";
+  }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  if (tip === "generous") {
+    return "Thank you so much."
+  }
+  else if (tip === "not as generous") {
+    return "Thank you."
+  }
+  else {
+    return "Bye."
+  }
 }
